1a687408d53ede948aefb414d7a188d743831e7d
Brian051 <brian051.work@gmail.com>
2025-01-30 13:19:10 +0100

    Módulo funcional sin integración real de tipos de daño

---
+++ implements/modules/reflect-damage/module.js
@@ -0,0 +1,38 @@
+
+Hooks.once("ready", () => {
+    // Llama a la función que configurará los ajustes del módulo
+    console.log("Reflect Damage Module está listo.");
+  
+    // Definir la configuración de tipo de daño predeterminado
+    game.settings.register("reflect-damage", "damageType", {
+      name: "Tipo de daño",
+      hint: "Selecciona el tipo de daño que se reflejará",
+      scope: "world",
+      config: true,
+      type: String,
+      choices: {
+        "radiant": "Radiant",
+        "acid": "Ácido",
+        "fire": "Fuego",
+        "cold": "Frío",
+        "poison": "Veneno",
+        "lightning": "Rayos",
+        "thunder": "Trueno",
+        "force": "Fuerza",
+        "necrotic": "Necrótico",
+        "psychic": "Psíquico"
+      },
+      default: "radiant",
+    });
+
+    game.settings.register("reflect-damage", "dices", {
+      name: "Tirada de dados",
+      hint: "Insertar la receta de dados",
+      scope: "world",
+      config: true,
+      type: String,
+      default: "1d8",
+    });
+  
+  });
+  
